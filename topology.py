93af5935f787e24784f0613f7c95f1bf90e5091b
Loedpipat <loedpipat.nsk@gmail.com>
2025-02-04 21:41:09 +0700

    Initial state for topology by using container

--- topology.py
+++ topology.py
@@ -1,52 +1,56 @@
 #!/usr/bin/python
-"""
-This is an example with Traffic Control for link weight.
-"""
+
 from containernet.net import Containernet
 from containernet.node import DockerSta
 from containernet.cli import CLI
 from containernet.term import makeTerm
 from mininet.log import info, setLogLevel
-
+from mininet.node import Controller, RemoteController
 
 def topology():
     net = Containernet()
 
+    info("*** Adding controller\n")
+    c0 = net.addController('c0', controller=RemoteController, ip='192.168.56.201', port=6633)
+    
+    info("*** Adding Access Points\n")
+    ap1 = net.addAccessPoint('ap1', ssid='new-ssid', mode='g', channel='1', position='10,20,0')
+
+    info("*** Adding OpenFlow switches\n")
+    s1 = net.addSwitch('s1')
+
     info('*** Adding docker containers\n')
     sta1 = net.addStation('sta1', ip='10.0.0.1', mac='00:02:00:00:00:01',
-                          cls=DockerSta, dimage="ubuntu:trusty", cpu_shares=50)
+                          cls=DockerSta, dimage="ubuntu:trusty", cpu_shares=20)
     sta2 = net.addStation('sta2', ip='10.0.0.2', mac='00:02:00:00:00:02',
-                          cls=DockerSta, dimage="ubuntu:trusty", cpu_shares=50)
-    ap1 = net.addAccessPoint('ap1', ssid='new-ssid', mode='g', channel='1')
-    c0 = net.addController('c0')
+                          cls=DockerSta, dimage="ubuntu:trusty", cpu_shares=20)
+    
+    info("*** Adding host\n")
+    h1 = net.addDocker('h1', ip='10.0.0.10', dimage="ubuntu:trusty")
 
     info('*** Configuring WiFi nodes\n')
     net.configureWifiNodes()
 
+    info('*** Creating links\n')
+    net.addLink(ap1, s1)
+
+    net.addLink(sta1, ap1)
+    net.addLink(sta2, ap1)
+
+    net.addLink(s1, h1)
+
     info('*** Starting network\n')
     net.start()
+    c0.start()
+    ap1.start([c0])
+    s1.start([c0])
 
-    # Update and install necessary packages in the Docker containers
-    makeTerm(sta1, cmd="bash -c 'apt-get update && apt-get install -y iproute2 net-tools;'")
-    makeTerm(sta2, cmd="bash -c 'apt-get update && apt-get install -y iproute2 net-tools;'")
-
-    # Connect stations to the access point
-    sta1.cmd('iw dev sta1-wlan0 connect new-ssid')
-    sta2.cmd('iw dev sta2-wlan0 connect new-ssid')
-
-    # Apply traffic control rules to control link weights
-    info('*** Configuring Traffic Control (TC) for link weights\n')
-
-    # Example: Apply 1 Mbps bandwidth limit between sta1 and ap1
-    sta1.cmd('tc qdisc add dev sta1-wlan0 root tbf rate 1mbit burst 32kbit latency 400ms')
-
-    # Example: Apply 2 Mbps bandwidth limit between sta2 and ap1
-    sta2.cmd('tc qdisc add dev sta2-wlan0 root tbf rate 2mbit burst 32kbit latency 400ms')
+    makeTerm(sta1, cmd="bash -c 'apt-get update && apt-get install iw wireless-tools ethtool iproute2 net-tools -y;'")
+    makeTerm(sta2, cmd="bash -c 'apt-get update && apt-get install iw wireless-tools ethtool iproute2 net-tools -y;'")
 
     info('*** Running CLI\n')
     CLI(net)
 
-    # Stop the network
     info('*** Stopping network\n')
     net.stop()
 
@@ -54,3 +58,4 @@ def topology():
 if __name__ == '__main__':
     setLogLevel('info')
     topology()
+
